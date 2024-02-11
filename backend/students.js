2c55ef9fc61060a19ac52da5a71683567c88e0b5
harshiempire <alleharshith@gmail.com>
2024-02-11 20:27:57 +0530

    creating the form

---
+++ backend/students.js
@@ -0,0 +1,65 @@
+const express = require("express");
+students = [
+  {
+    id: 1,
+    first_name: "Alejandra",
+    last_name: "Byatt",
+    email: "abyatt0@geocities.com",
+  },
+  {
+    id: 2,
+    first_name: "Brigitta",
+    last_name: "Jindracek",
+    email: "bjindracek1@g.co",
+  },
+  {
+    id: 3,
+    first_name: "Steffi",
+    last_name: "Gonsalo",
+    email: "sgonsalo2@oracle.com",
+  },
+  {
+    id: 4,
+    first_name: "Emanuele",
+    last_name: "Starbeck",
+    email: "estarbeck3@alexa.com",
+  },
+  {
+    id: 5,
+    first_name: "Trenton",
+    last_name: "Johananoff",
+    email: "tjohananoff4@cnn.com",
+  },
+  {
+    id: 6,
+    first_name: "Ariella",
+    last_name: "Champain",
+    email: "achampain5@about.com",
+  },
+  {
+    id: 7,
+    first_name: "Florri",
+    last_name: "Mead",
+    email: "fmead6@ask.com",
+  },
+  {
+    id: 8,
+
+    first_name: "Tamiko",
+    last_name: "Noakes",
+    email: "tnoakes7@t-online.de",
+  },
+  {
+    id: 9,
+    first_name: "Tye",
+    last_name: "Cowper",
+    email: "tcowper8@ycombinator.com",
+  },
+  {
+    id: 10,
+    first_name: "Bonita",
+    last_name: "Karpets",
+    email: "bkarpets9@theguardian.com",
+  },
+];
+module.exports = students;
